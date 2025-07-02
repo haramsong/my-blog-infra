9f2b5b6a28c657753b6fe2cff85b0b2338071638
haramsong <alakazam1324@gmail.com>
2025-07-02 11:29:30 +0900

    🎨 assume role 방식으로 변경

--- provider.tf
+++ provider.tf
@@ -12,4 +12,7 @@ terraform {
 provider "aws" {
   region  = "ap-northeast-2"
   profile = var.profile
+  assume_role {
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.role_name}"
+  }
 }
