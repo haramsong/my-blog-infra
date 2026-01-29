392b25d1c4e1eaf66d69f5e205582e0adcf344f8
haramsong <alakazam1324@gmail.com>
2026-01-29 16:02:00 +0900

    🧱 trailing slash 없으면 리다이렉트

--- function.js
+++ function.js
@@ -1,11 +1,22 @@
-var regexExpr = /^\/.+(\.\w+$)/;
+var defined = /^\/.+(\.\w+$)/;
 
 function handler(event) {
   var request = event.request;
-  var olduri = request.uri;
+  var uri = request.uri;
 
-  if (!regexExpr.test(olduri)) {
-    request.uri = olduri.replace(/\/?$/, '/') + 'index.html';
+  // 파일 확장자가 없고 trailing slash가 없으면 리다이렉트
+  if (!defined.test(uri) && !uri.endsWith('/')) {
+    console.log('Redirect [' + uri + '] with trailing slash [' + uri + '/]');
+    return {
+      statusCode: 301,
+      statusDescription: 'Moved Permanently',
+      headers: { location: { value: uri + '/' } }
+    };
+  }
+
+  // trailing slash가 있으면 index.html 추가
+  if (!defined.test(uri)) {
+    request.uri = uri + 'index.html';
     console.log('Request for [' + olduri + '] rewritten to [' + request.uri + ']');
   }
 
